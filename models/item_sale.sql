2baf0bb9288c5f9fa4aadeea419abd2088991915
anu t <anushatalapaneni98@gmail.com>
2023-12-25 08:20:26 +0000

    item_sale table

--- models/item_sale.sql
+++ models/item_sale.sql
@@ -1,4 +1,6 @@
-with item_sale as (
+{{ config(materialized='table') }}
+
+with item_sale1 as (
 select i_category,sum(i_current_price),sum(i_wholesale_cost) from RAW.ITEM
 group by i_category)
-select * from item_sale
+select * from item_sale1
